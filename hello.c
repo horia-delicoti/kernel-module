0305ac4cf958ecf6c6f97c736a235ad6656841aa
Horia Delicoti <horia.delicoti@gmail.com>
2016-05-25 11:13:47 +0300

    Changed hello world messages and header.

--- hello.c
+++ hello.c
@@ -1,20 +1,5 @@
 /******************************************************************************
- *
- *   Copyright (C) 2011  Intel Corporation. All rights reserved.
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; version 2 of the License.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * Functions that print messages when the kernel module is loaded.  
  *****************************************************************************/
 
 #include <linux/init.h>
@@ -23,13 +8,13 @@ MODULE_LICENSE("GPL");
 
 int hello_init(void)
 {
-	printk(KERN_ALERT "Hello World!\n");
+	printk(KERN_ALERT "HelloWorld kernel module has loaded!\n");
 	return 0;
 }
 
 void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye Cruel World!\n");
+	printk(KERN_ALERT "HelloWorld kernel module is removed!\n");
 }
 
 module_init(hello_init);
